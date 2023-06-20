3716666ed3362234d4551341096139cda54068a3
vinicius de paiva <cpinho@tecnologia-unica.com.br>
2023-06-20 12:35:28 -0300

    Projeto MVC

---
+++ CadastroCliente/CadastroCliente/Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroCliente.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+
+    }
+}
